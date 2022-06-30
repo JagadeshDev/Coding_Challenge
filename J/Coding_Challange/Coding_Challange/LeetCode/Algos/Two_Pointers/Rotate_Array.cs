22cd4e4eee9a2b5e0da57c98144d80ade9bc138d
Jagadesh <jagadesh.julapalli@gmail.com>
2022-06-30 22:07:20 +0530

    Two pointers

---
+++ J/Coding_Challange/Coding_Challange/LeetCode/Algos/Two_Pointers/Rotate_Array.cs
@@ -0,0 +1,20 @@
+﻿namespace Coding_Challange.LeetCode.Algos.Two_Pointers
+{
+    public class Rotate_Array
+    {
+        public int[] Rotate(int[] nums, int k)
+        {
+            int[] tmp = new int[nums.Length];
+            int magic = nums.Length - k;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i < k)
+                    tmp[i] = nums[i + magic];
+                else
+                    tmp[i] = nums[i - k];
+            }
+            nums = tmp;
+            return tmp;
+        }
+    }
+}
