c117ffcf71a6e2413d4653492658d9da18ac82d9
Jagadesh <jagadesh.julapalli@gmail.com>
2022-06-30 23:19:38 +0530

    solved two more challenges

---
+++ J/Coding_Challange/Coding_Challange/LeetCode/Algos/Two_Pointers/Two_Sum_II_Input_Array_Is_Sorted.cs
@@ -0,0 +1,35 @@
+﻿namespace Coding_Challange.LeetCode.Algos.Two_Pointers
+{
+    public class Two_Sum_II_Input_Array_Is_Sorted
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int[] output = new int[2];
+
+            int start = 0, len = numbers.Length - 1, end = len;
+            for (int i = 0; i <= len; i++)
+            {
+                int sum = (numbers[start] + numbers[end]);
+
+                if (sum > target)
+                {
+                    end--;
+                    continue;
+                }
+                else if (sum < target)
+                {
+                    start++;
+                    continue;
+                }
+
+                else
+                {
+                    output[0] = start + 1;
+                    output[1] = end + 1;
+                    break;
+                }
+            }
+            return output;
+        }
+    }
+}
